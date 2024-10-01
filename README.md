4f4bd4d6c4c4c942ead7ddaa98faa17dc5041796
LeeT27 <git@stackblitz.com>
2024-10-01 10:41:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# catch1
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/LeeT27/catch1)
---
+++ main.js
@@ -0,0 +1,38 @@
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: 'game-container',
+  background: '#191970',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false,
+    },
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
+};
+var player;
+const game = new Phaser.Game(config);
+function preload() {
+  // Load assets here (e.g., images, sounds)
+  // this.load.image('sky', 'assets/sky.png');
+  this.load.image('player', 'assets/player.png');
+}
+
+function create() {
+  // Add game objects here
+  // this.add.image(400, 300, 'sky');
+  player = this.physics.add.image(0, 500, 'player');
+  player.setCollideWorldBounds(true);
+}
+
+function update() {
+  const mouseX = this.input.x;
+  player.x = mouseX;
+}
