3fd7df6e106597483c09c11abb73153d9499a3ee
Juno <138246438+LeeT27@users.noreply.github.com>
2024-10-06 22:51:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -23,6 +23,10 @@ function preload() {
   // Load assets here (e.g., images, sounds)
   // this.load.image('sky', 'assets/sky.png');
   this.load.image('player', 'assets/player.png');
+  this.load.spritesheet('rock', 'assets/rock.png', {
+    frameWidth: 85,
+    frameHeight: 100,
+  });
 }
 
 function create() {
@@ -30,17 +34,23 @@ function create() {
   // this.add.image(400, 300, 'sky');
   player = this.physics.add.image(0, 500, 'player');
   player.setCollideWorldBounds(true);
+  this.anims.create({
+    key: 'loop',
+    frames: this.anims.generateFrameNumbers('rock', { start: 0, end: 6 }), // Adjust start and end as per your spritesheet
+    frameRate: 10,
+    repeat: -1, // Loop forever
+  });
+  let rock = this.add.sprite(100, 100, 'rock');
+  rock.play('loop');
 }
 
 function update() {
   const mouseX = this.input.x;
-  if (mouseX<player.width/2){
-    player.x=player.width/2;
-  }
-  else if(mouseX>game.config.width-player.width/2){
-    player.x=game.config.width-player.width/2;
-  }
-  else{
+  if (mouseX < player.width / 2) {
+    player.x = player.width / 2;
+  } else if (mouseX > game.config.width - player.width / 2) {
+    player.x = game.config.width - player.width / 2;
+  } else {
     player.x = mouseX;
   }
 }
